512785cc19e8cb59711e29874f31e97093a986dc
ElBomberitoFeliz <jeisonortiz0016@gmail.com>
2020-10-13 18:13:30 -0500

    Correción email

--- forms/contact.php
+++ forms/contact.php
@@ -1,38 +1,20 @@
 <?php
-$destinatario = "jeisonortiz0016@gmail.com";
-$asunto = "Este mensaje es de prueba";
-$cuerpo = ' 
-<html> 
-<head> 
-   <title>Prueba de correo</title> 
-</head> 
-<body> 
-<h1>Hola amigos!</h1> 
-<p> 
-<b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
-</p> 
-</body> 
-</html> 
-';
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
 
-//para el envío en formato HTML
-$headers = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'jeisonortiz0016@gmail.com';
 
-//dirección del remitente
-$headers .= "From: Miguel Angel Alvarez <pepito@desarrolloweb.com>\r\n";
 
-//dirección de respuesta, si queremos que sea distinta que la del remitente
-$headers .= "Reply-To: mariano@desarrolloweb.com\r\n";
+  $from_name = $_POST['name'];
+  $from_email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $headers = "From:".$from_email;
 
-//ruta del mensaje desde origen a destino
-$headers .= "Return-path: holahola@desarrolloweb.com\r\n";
-
-//direcciones que recibián copia
-$headers .= "Cc: maria@desarrolloweb.com\r\n";
-
-//direcciones que recibirán copia oculta
-$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n";
-
-mail($destinatario,$asunto,$cuerpo,$headers)
+  $asunto = 'De:' .$from_name;
+ mail($receiving_email_address,$asunto,$subject,$headers)
 ?>
